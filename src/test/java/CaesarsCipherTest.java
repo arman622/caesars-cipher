93ef1db235a02916d855f63594f3dfa1d32f25d8
arman <armanthaque@gmail.com>
2022-02-05 18:00:37 -0500

    First Version

---
+++ src/test/java/CaesarsCipherTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CaesarsCipherTest {
+
+    private CaesarsCipher caesarsCipher = new CaesarsCipher();
+
+    @Test
+    void testCipheredMessageWithOffsetof12(){
+        assertEquals("tai mdq kag pauzs fapmk", caesarsCipher.cipher("how are you doing today", 12));
+    }
+    @Test
+    void testEmptyString(){
+        assertEquals("", caesarsCipher.cipher("", 12));
+    }
+
+    //fail test case
+//    @Test
+//    void testCipheredMessageWithOffsetof11(){
+//        assertEquals("tai mdq kag pauzs fapmk", caesarsCipher.cipher("how are you doing today", 11));
+//    }
+
+}
