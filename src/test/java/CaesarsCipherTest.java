8d31fd09c5fa4f8931f059e9172584258062c613
arman <armanthaque@gmail.com>
2022-02-05 21:33:23 -0500

    Added New Test in feature 1

--- src/test/java/CaesarsCipherTest.java
+++ src/test/java/CaesarsCipherTest.java
@@ -16,9 +16,12 @@ class CaesarsCipherTest {
     }
 
     //fail test case
-//    @Test
-//    void testCipheredMessageWithOffsetof11(){
-//        assertEquals("tai mdq kag pauzs fapmk", caesarsCipher.cipher("how are you doing today", 11));
-//    }
+    @Test
+    void testCipheredMessageWithOffsetof11(){
+        assertEquals("tai mdq kag pauzs fapmk", caesarsCipher.cipher("how are you doing today", 11));
+    }
+
+
+
 
 }
